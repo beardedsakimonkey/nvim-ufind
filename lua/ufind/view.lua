0cf0b64547aa84f77eddadadfa8f03872bad57e6
beardedsakimonkey <sakimonkey@protonmail.com>
2022-11-06 17:33:36 -0800

    set window borders, adjust window highlights

--- lua/ufind/view.lua
+++ lua/ufind/view.lua
@@ -3,27 +3,28 @@ local api = vim.api
 local PROMPT = '> '
 
 local function get_win_layouts()
+    local border = true  -- TODO: make configurable
     -- Size of the window
     local height = math.floor(vim.go.lines * 0.8)
     local width = math.floor(vim.go.columns * 0.7)
     -- Position of the window
     local row = math.ceil((vim.go.lines - height) / 2)
     local col = math.ceil((vim.go.columns - width) / 2)
-    -- Height of the input window
-    local input_height = 1
+    -- Height of the input window, including borders
+    local input_height = border and 3 or 1
     local input_win = {
         relative = 'editor',
         row = row,
         col = col,
-        width = width,
+        width = width - (border and 2 or 0),
         height = 1,
     }
     local result_win = {
         relative = 'editor',
         row = (input_height + row),
         col = col,
-        width = width,
-        height = (height - input_height),
+        width = width - (border and 2 or 0),
+        height = (height - input_height - (border and 2 or 0)),
     }
     return input_win, result_win
 end
@@ -34,16 +35,17 @@ local function create_wins(input_buf, result_buf)
     local input_win = api.nvim_open_win(
         input_buf,
         true,  -- enter
-        vim.tbl_extend('force', {style = 'minimal'}, input_win_layout)
+        vim.tbl_extend('force', {style = 'minimal', border = 'single'}, input_win_layout)
     )
     local result_win = api.nvim_open_win(
         result_buf,
         false,  -- enter
-        vim.tbl_extend('force', {style = 'minimal', focusable = false}, result_win_layout)
+        vim.tbl_extend('force', {style = 'minimal', border = 'single', focusable = false}, result_win_layout)
     )
     vim.wo[result_win].cursorline = true
-    -- vim.wo[result_win].winhighlight = 'CursorLine:PmenuSel'
+    -- TODO: make configurable
     vim.wo[result_win].winhighlight = 'NormalFloat:Normal'
+    vim.wo[input_win].winhighlight = 'NormalFloat:Normal'
     return input_win, result_win
 end
 
@@ -69,7 +71,6 @@ end
 
 return {
     PROMPT = PROMPT,
-    get_win_layouts = get_win_layouts,
     create_wins = create_wins,
     handle_vimresized = handle_vimresized,
     create_input_buf = create_input_buf,
