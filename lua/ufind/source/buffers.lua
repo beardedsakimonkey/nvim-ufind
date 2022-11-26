bf55f9df17c5070ead7c64fe220b801e9b4daec8
beardedsakimonkey <sakimonkey@protonmail.com>
2022-11-25 17:55:34 -0800

    feat: add ufind.sources

---
+++ lua/ufind/source/buffers.lua
@@ -0,0 +1,27 @@
+---@return string[]
+local function buffers_source()
+    local origin_buf = vim.api.nvim_win_get_buf(0)
+
+    local bufs = vim.tbl_filter(function(buf)
+        local name = vim.fn.bufname(buf)
+        return name ~= ''                           -- buffer is named
+            and not vim.startswith(name, 'man://')  -- not a manpage
+            and buf ~= origin_buf                   -- not the current buffer
+            and vim.fn.buflisted(buf) == 1          -- buffer is listed
+            and vim.fn.bufexists(buf) == 1          -- buffer exists
+    end, vim.api.nvim_list_bufs())
+
+    -- Sort by last used
+    table.sort(bufs, function(a, b)
+        return vim.fn.getbufinfo(a)[1].lastused > vim.fn.getbufinfo(b)[1].lastused
+    end)
+
+    -- Get buffer names
+    local bufnames = vim.tbl_map(function(buf)
+        return vim.fn.bufname(buf)
+    end, bufs)
+
+    return bufnames
+end
+
+return buffers_source
---
+++ lua/ufind/source/oldfiles.lua
@@ -0,0 +1,13 @@
+---@return string[]
+local function oldfiles_source()
+    local fnames = vim.tbl_filter(function(fname)
+        return fname
+            and vim.fn.empty(vim.fn.glob(fname)) == 0  -- not wildignored
+            and vim.fn.isdirectory(fname) == 0         -- not a directory
+            and not vim.startswith(fname, 'man://')    -- not a manpage
+    end, vim.v.oldfiles)
+
+    return fnames
+end
+
+return oldfiles_source
